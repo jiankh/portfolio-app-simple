798f3802f0025502497bddeca8e0d39b17228adf
jiankh <131821436+jiankh@users.noreply.github.com>
2023-10-02 20:16:14 -0400

    added function to populate the data for the projects when projects is clicked

---
+++ projectData.js
@@ -0,0 +1,76 @@
+
+
+const Projects = [
+    {
+        title: "Calculator",
+        img: "",
+        description: "Full functional simple calculator developed using HTML/CSS/JavaScript. Handles the basic mathemical equations.",
+        codeLink: "https://github.com/jiankh/calculator",
+        previewLink: "https://jiankh.github.io/calculator/"
+    },
+    {
+        title: "Bakery Site",
+        img: "",
+        description: "blah blah",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+    {
+        title: "",
+        img: "",
+        description: "",
+        codeLink: "",
+        previewLink: ""
+    },
+]
+
+export default Projects
---
+++ script.js
@@ -0,0 +1,29 @@
+import Projects from "./projectData.js"
+
+const projectContainer = document.querySelector(".project-container")
+
+function createProject(img, title, description, codeLink, previewLink) {
+    const project = document.createElement("div")
+    project.classList.add("project")
+    project.innerHTML = `
+    <div class="project-img"><img src="${img}"></div>
+    <div class="project-description">
+        <div class="project-title">${title}</div>
+        <div class="description"> ${description} </div>
+        <div class="buttons">
+            <input type="button" onclick="location.href='${codeLink}';" value="View Code" />
+            <input type="button" onclick="location.href='${previewLink}';" value="Live Preview" />
+        </div>
+    </div>
+    `
+
+    projectContainer.appendChild(project)
+}
+
+
+
+Projects.forEach((project) => {
+    console.log("Hello")
+    createProject(project.img, project.title, project.description, project.codeLink, project.previewLink)
+})
+
