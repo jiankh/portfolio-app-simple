fa6710493d414a7f618acfae6b7c1ad46310f6ea
jiankh <131821436+jiankh@users.noreply.github.com>
2023-10-03 08:37:24 -0400

    added screenshot of project into image folder

--- projectData.js
+++ projectData.js
@@ -9,60 +9,60 @@ const Projects = [
         previewLink: "https://jiankh.github.io/calculator/"
     },
     {
-        title: "Bakery Site",
+        title: "Bakery Website",
         img: "",
         description: "Frontend React website for a mock restaurant with several functional tabs. The main focus of this project was to modulize the elements of the site for better reusability. The menu items are also stored in an array of object that can easily by editted to add and remove menu items.",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/sprigsbakery",
+        previewLink: "https://jiankh.github.io/sprigsbakery/"
     },
     {
         title: "Tic Tac Toe",
         img: "",
         description: "Javascript application of the classic game Tic Tac Toe. Features Multiplayer and Single player. Includes an upbeatable AI when playing singleplayer mode, made with the MiniMax Algorithm",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/tictactoe",
+        previewLink: "https://jiankh.github.io/tictactoe/"
     },
     {
         title: "Sketch Book",
         img: "",
         description: "A drawing board that functions similarily to MS Paint. Allows the user to choose the size of the canvas and also has a special rainbow effect drawing. The focus of this project was DOM manipulation using JavaScript.",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/sketchbook",
+        previewLink: "https://jiankh.github.io/sketchbook/"
     },
     {
         title: "Pygame Platformer",
         img: "",
         description: "[WIP] Pygame based game. Platformer adventure game with a main character similar to the fairy tale of Little Red Riding Hood needs to navigate through the forest to reach a destination.",
-        codeLink: "",
+        codeLink: "https://github.com/jiankh/RedHood",
         previewLink: ""
     },
     {
         title: "Rock Paper Scissor",
         img: "",
         description: "A classic game of Rock Paper Scissors. Game is first to get 5 wins.",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/RPS_game",
+        previewLink: "https://jiankh.github.io/RPS_game/"
     },
     {
         title: "Task To-Do List",
         img: "",
         description: "[WIP] A To-Do List application. Features options to create a section and add tasks to the specific section that can be checked off. The focus of this project was to use LocalStorage to manage input information and organize the project in Modules while using Webpack.",
-        codeLink: "",
+        codeLink: "https://github.com/jiankh/to-do-list",
         previewLink: ""
     },
     {
         title: "Library App",
         img: "",
         description: "An application that lets user save information about books that they are interested in. The user can also change and see the status of the books if they finished reading it or not.",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/libraryProject",
+        previewLink: "https://jiankh.github.io/libraryProject/"
     },
     {
         title: "Dashboard",
         img: "",
         description: "This is a simple sample Dashboard created to focus on UX and Grid Layouts in CSS",
-        codeLink: "",
-        previewLink: ""
+        codeLink: "https://github.com/jiankh/dashboardSample",
+        previewLink: "https://jiankh.github.io/dashboardSample/"
     }   
 ]
 
